d36e0a4fdb6ccf4a6b9ef119e809ae5418ef94c0
Leonid <smolyanov.lm@gmail.com>
2023-11-08 18:10:16 +0300

    сделал проверку на одинаковые числа

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,34 +5,63 @@
 
 #define N 4
 
-int main()
+// напечетать число (которое в массиве)
+void print_ar(char ar[], int n)
 {
-    setlocale(LC_ALL, "Russian");
+    for (int i = 0; i < n; i++)
+        printf("%c", ar[i]);
+}
 
-    char mech[N];
+// генерируем число и записываем в массив
+void rand_4(char ar[], int n)
+{
+    int k = rand() % 9999;
+//  std::cout << "k= " << k << std::endl;
 
-    srand(time(NULL));
-
-    for (int i = 1; i < 15; i++)
-        rand();
-
-    int n = rand() % 9999;
-
-    std::cout << "n= " << n << std::endl;
-
-    for (int i = N-1 ; i >= 0; i--)
-  
+    for (int i = N - 1; i >= 0; i--)
     {
-        mech[i] = '0' + n % 10;
-        n = n / 10;
-
+        ar[i] = '0' + k % 10;
+        k = k / 10;
     }
-    printf("***");
-    for (int i = 0; i < N; i++)
-        printf("%c", mech[i]);
+}
 
-   // for (int i = 1; i < 20; i++)
-   // std::cout << rand()% 9999 << std::endl;
- 
+bool SameDigits(char ar[], int n)//создаем цикл для сравнивания одинаковых чисел
+{
+    int count = 0;
+    for (int i = 0; i < N; i++)
+        for (int j = i + 1; j < N; j++)  
+        {
+            if (ar[i] == ar[j])
+                count++;
+        }
+
+    return (count != 0);
+  //  std::cout << std::endl << "count = " << count << std::endl;
+}
+
+
+    int main()
+    {
+        setlocale(LC_ALL, "Russian");
+
+        char mech[N];
+
+        // инициализация ГСЧ
+        srand(time(NULL));
+        for (int i = 1; i < 15; i++)
+            rand();
+        // **********
+            
+        // повторяем пока не будет сгенерировано число с разными цифрами
+        do {
+            rand_4(mech, N);
+            print_ar(mech, N);
+            printf("\n");
+        } while (SameDigits(mech, N)); //генерируем число, проверяем нет ли одинаковых цифр тогда печатаем
+                                       // если есть одинаковые цифры,цикл повторяется
+  //    printf("***");
+        print_ar(mech, N);
+      
 
 }
+
