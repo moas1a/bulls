bf1fcd5baa8f6263f19c79464659103475445749
Leonid <smolyanov.lm@gmail.com>
2023-11-08 20:09:32 +0300

    доделал программу быки и коровы

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -9,21 +9,25 @@
 void print_ar(char ar[], int n)
 {
     for (int i = 0; i < n; i++)
-        printf("%c", ar[i]);
+         printf("%c", ar[i]);
 }
 
+
+// Распихиваем цифры из числа value в массив ar
+void Move2Array(int value, char ar[], int n)
+{
+    for (int i = N - 1; i >= 0; i--)
+    {
+        ar[i] = '0' + value % 10;
+        value = value / 10;
+    }
+}
 // генерируем число и записываем в массив
 void rand_4(char ar[], int n)
 {
-    int k = rand() % 9999;
-//  std::cout << "k= " << k << std::endl;
-
-    for (int i = N - 1; i >= 0; i--)
-    {
-        ar[i] = '0' + k % 10;
-        k = k / 10;
-    }
+    Move2Array(rand() % 9999, ar, n);
 }
+
 
 bool SameDigits(char ar[], int n)//создаем цикл для сравнивания одинаковых чисел
 {
@@ -36,32 +40,91 @@ bool SameDigits(char ar[], int n)//создаем цикл для сравнив
         }
 
     return (count != 0);
-  //  std::cout << std::endl << "count = " << count << std::endl;
 }
 
+int main()
+{
+    setlocale(LC_ALL, "Russian");
 
-    int main()
+    char mech[N];
+    char ours[N];
+
+    int raz = 0;
+
+    // инициализация ГСЧ
+    srand(time(NULL));
+    for (int i = 1; i < 15; i++)
+        rand();
+    // **********
+
+  // повторяем пока не будет сгенерировано число с разными цифрами
+    do {
+        rand_4(mech, N);
+    } while (SameDigits(mech, N)); //генерируем число, проверяем нет ли одинаковых цифр тогда печатаем
+
+    // print_ar(mech, N); //выводит ответ на экран
+   //  printf("\n");
+
+    int nOurs;
+
+    int bools = 0; // быки
+    int cows = 0;  // коровы
+
+
+    do {
+        //Заставляем пользователя ввести целое число из N различных цифр от 123 до 9876
+        // 0 означает конец игры
+
+    std::cout << "Введите число из " << N << " цифр";
+    std::cin >>  nOurs;
+    if (nOurs == 0)
     {
-        setlocale(LC_ALL, "Russian");
+        std::cout << "Вам надоело играть!";
+        break;
+    }
+    if (nOurs > 9876)
+    {
+        std::cout << "Вы ввели слишком большое число \n";
+        continue;
+    }
+    if (nOurs < 123)
+    {
+        std::cout << "Вы ввели слишком маленькое число \n";
+        continue;
+    }
 
-        char mech[N];
+  
+    // запихиваем введенное число в массив ours[]
+        Move2Array(nOurs, ours, N);
 
-        // инициализация ГСЧ
-        srand(time(NULL));
-        for (int i = 1; i < 15; i++)
-            rand();
-        // **********
-            
-        // повторяем пока не будет сгенерировано число с разными цифрами
-        do {
-            rand_4(mech, N);
-            print_ar(mech, N);
-            printf("\n");
-        } while (SameDigits(mech, N)); //генерируем число, проверяем нет ли одинаковых цифр тогда печатаем
-                                       // если есть одинаковые цифры,цикл повторяется
-  //    printf("***");
-        print_ar(mech, N);
-      
+        if (SameDigits(ours,N))
+        {
+            std::cout << "Нельзя вводить одинаковые цифры!";
+            continue;
+        }
+
+        // пользователь ввел число 
+        // которое удовлетворяет всем нашим условиям
+
+        raz++; // количество попыток угадывания числа
+
+        // считаем быков и коров
+        bools = 0; // быки
+        cows = 0;  // коровы
+        for (int i = 0; i < N; i++)
+        {
+            if (mech[i] == ours[i])
+                bools++;
+            for (int j = 0; j < N; j++)
+                if (mech[i] == ours[j])
+                    cows++;
+        }
+        cows -= bools;
+
+
+        std::cout << "Попытка №: " << raz << "Быков " << bools << "; Коров " << cows << std::endl;
+
+
+ } while (bools != 4);
 
 }
-
